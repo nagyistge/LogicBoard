58c54566caf98f7de834e681620191b631ff5199
scutxieyun <yun.xie@MMGZ-LP-12-01.shanghai.marelli.it>
2015-03-10 21:30:41 +0800

    use the window for project tree

--- js/proj.tree.js
+++ js/proj.tree.js
@@ -6,16 +6,16 @@ function ProjectTree(con){
                         children:[
                             {
                                 label:"Doors",
-                                type:"group",
+                                type:"folder",
                                 children:[
                                     {
                                         label:"normal_case",
-                                        type:"TC",
+                                        type:"tc",
                                         id:12
                                     },
                                     {
                                         label:"door and pwl",
-                                        type:"sequence",
+                                        type:"seq",
                                         id:13
                                     }
                                 ]
@@ -23,10 +23,57 @@ function ProjectTree(con){
                         ]
                        }];
   var project_tree = $(con).tree(
-                  {data:project,autoOpen:true,dragAndDrop:true});
-        project_tree.bind("tree.click",function(evt){
-                        var node = evt.node;
-                        if(node.type == "sequence") win_manager.createSeqTab(node.name);
-                        if(node.type == "TC") win_manager.createTCTab(node.name);
-                        });
+                  {data:project,autoOpen:true,dragAndDrop:true,
+                   onCreateLi:function(node,$li){
+                                var $menu = $("<span></span>").addClass("tree-menu").append($("<div>Menu</div>").addClass("menu-bar"));
+                                $menu.append(createContextMenu(node));
+                                $li.find('.jqtree-element').append($menu);
+                            },
+                   }
+                   );
+  $(con).on("click",".pulldown-menu li",function(evt){
+                                        var command = evt.target.id;
+                                        var node = $(evt.target).parent().data();
+                                        node_func_tab[command](node);
+                                    });
+   var node_func_tab = {
+    "delete":delete_node,
+    "create_sub_folder":create_sub_folder,
+    "create_tc":create_tc,
+    "create_seq":create_seq,
+    "open":open_node
+   };
+   function delete_node(node){
+   }
+   function create_sub_folder(node){
+    var parent_node = project_tree.tree('getNodeById', node.id);
+    project_tree.tree("appendNode",{label:"new folder",id:34534,type:"folder"},parent_node);
+   }
+   function create_tc(node){
+   }
+   function create_seq(node){
+   
+   }
+   function open_node(node){
+    if(node.type == "tc"){
+        win_manager.createTCTab(node.name);
+    }
+    if(node.type == "seq"){
+        win_manager.createSeqTab(node.name);
+    }
+   }
+   function createContextMenu(node){
+       var $menu = $("<div></div>").addClass("pulldown-menu").data(node);
+       if(node.type != "project")
+            $menu.append("<li id='delete'>delete</li>");
+       if(node.type == "folder" || node.type == "project"){
+            $menu.append("<li id='create_sub_folder'>create sub folder</li>")
+                 .append("<li id='create_tc'>create TC</li>")
+                 .append("<li id='create_seq'>create sequence</li>");
+       }
+       if(node.type == "tc" || node.type == "seq"){
+            $menu.append("<li id='open'>open</li>");
+       }
+       return $menu;
+   }
 }
